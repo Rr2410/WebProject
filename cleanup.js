1104a9df45072f7ec39cf22bc267380a0c530765
Rr2410 <123rr456aa@gmail.com>
2024-07-27 14:45:35 +0300

    added new pages and navbar

--- cleanup.js
+++ cleanup.js
@@ -2,7 +2,7 @@ const cron = require('node-cron');
 const mongoose = require('mongoose');
 const User = require('./userSchema'); 
 
-
+// this file periodically removes expired timetable entries from the database
 mongoose.connect('mongodb://localhost:27017/DBweb', { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('Connected to MongoDB'))
   .catch(err => console.error('Could not connect to MongoDB', err));
